11e5f1af63ab934574e1aef6050b71a28728f5b4
Jason Barto <jason.p.barto@gmail.com>
2025-10-20 01:21:41 +0100

    removed output param from buildx

--- cicd/build.sh
+++ cicd/build.sh
@@ -6,6 +6,5 @@ IMAGE_NAME=gobot:${RELEASE_NAME:-'vLocal'}
 
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
-    --output "type=docker,push=false" \
     --tag ${IMAGE_NAME} \
     .
