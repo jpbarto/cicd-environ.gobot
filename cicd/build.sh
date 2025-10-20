6416f11134353a04cb54bbc7f8e2ea4dbf6072be
Jason Barto <jason.p.barto@gmail.com>
2025-10-20 01:24:14 +0100

    trying to get container build working in github

--- cicd/build.sh
+++ cicd/build.sh
@@ -4,7 +4,11 @@ set -e
 
 IMAGE_NAME=gobot:${RELEASE_NAME:-'vLocal'}
 
-docker buildx build \
+docker buildx create \
+  --name container-builder \
+  --driver docker-container \
+  --bootstrap --use && \
+  docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --tag ${IMAGE_NAME} \
     .
