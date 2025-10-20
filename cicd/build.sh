1da7fe7abf7ba66883bbce21b1f63f47899c17da
Jason Barto <jason.p.barto@gmail.com>
2025-10-20 01:52:52 +0100

    added multi arch support to code delivery

--- cicd/build.sh
+++ cicd/build.sh
@@ -4,15 +4,6 @@ set -e
 
 IMAGE_NAME=gobot:${RELEASE_NAME:-'vLocal'}
 
-if ! `docker buildx inspect container-builder >/dev/null 2>&1`; then
-    docker buildx create \
-      --name container-builder \
-      --driver docker-container \
-      --bootstrap --use
-fi
-
 docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  --load \
   --tag ${IMAGE_NAME} \
   .
--- cicd/deliver.sh
+++ cicd/deliver.sh
@@ -2,4 +2,19 @@
 
 set -e
 
+IMAGE_NAME=gobot:${RELEASE_NAME:-'vLocal'}
+
+if ! `docker buildx inspect container-builder >/dev/null 2>&1`; then
+    docker buildx create \
+      --name container-builder \
+      --driver docker-container \
+      --bootstrap --use
+fi
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --output type=oci,dest=gobot.tgz \
+  --tag ${IMAGE_NAME} \
+  .
+
 helm package helm/src/gobot -d helm
