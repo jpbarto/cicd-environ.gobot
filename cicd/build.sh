866a569a5fd21024bcce529c49b330ed0a99810d
Jason Barto <jason.p.barto@gmail.com>
2025-10-20 01:34:55 +0100

    added more logic to the build script

--- cicd/build.sh
+++ cicd/build.sh
@@ -4,11 +4,15 @@ set -e
 
 IMAGE_NAME=gobot:${RELEASE_NAME:-'vLocal'}
 
-docker buildx create \
-  --name container-builder \
-  --driver docker-container \
-  --bootstrap --use && \
-  docker buildx build \
-    --platform linux/amd64,linux/arm64 \
-    --tag ${IMAGE_NAME} \
-    .
+if [ "$(docker buildx ls | grep docker-container)" -eq 1 ]; then
+    docker buildx create \
+      --name container-builder \
+      --driver docker-container \
+      --bootstrap --use
+fi
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --load \
+  --tag ${IMAGE_NAME} \
+  .
