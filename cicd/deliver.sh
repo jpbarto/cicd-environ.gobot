3cd63c456f501080babd37b552f6b96736db52c4
Jason Barto <jason.p.barto@gmail.com>
2025-10-24 15:54:33 +0100

    added more checks for a gh token to avoid error

--- cicd/deliver.sh
+++ cicd/deliver.sh
@@ -13,9 +13,13 @@ if ! `docker buildx inspect container-builder >/dev/null 2>&1`; then
       --bootstrap --use
 fi
 
-CR_PAT=${CR_PAT:-`gh auth token`}
-GHCR_USER=${GH_ACTOR:-'jpbarto'}
+if [ -z "${GITHUB_TOKEN}" ]; then
+    if [ -z "${CR_PAT}" ]; then
+        CR_PAT=`gh auth token || echo no token found`
+    fi
+fi
 GHCR_TOKEN=${GITHUB_TOKEN:-$CR_PAT}
+GHCR_USER=${GH_ACTOR:-'jpbarto'}
 echo Logging into Docker Registry with $GHCR_USER and token $GHCR_TOKEN
 echo $GHCR_TOKEN | docker login ghcr.io -u $GHCR_USER --password-stdin
 docker buildx build \
